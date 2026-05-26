62c3ff1c8f67461541ce990275e11e3b566f45a2
one <one@url.com>
2026-05-26 18:27:14 +0200

    computer component added, no animations

--- src/components/FloppyDisk.jsx
+++ src/components/FloppyDisk.jsx
@@ -6,6 +6,7 @@ export default function FloppyDisk({ title, bgColor, acColor }) {
   
    useGSAP(() => {
     const tl = gsap.timeline();
+    gsap.set(containerRef.current, {scale: 0.6})
     tl.from(containerRef.current, {
       x: -100,
       duration: 1,
@@ -15,13 +16,13 @@ export default function FloppyDisk({ title, bgColor, acColor }) {
   },{ scope: containerRef })
   return (
     <>
-      <div ref={containerRef} className={`floppyDisk-container flex ${bgColor} flex-col w-26 h-29 m-20 p-2 rounded-tr-xl rounded-sm gap-3 items-center pt-0 pb-0 box-border`}>
-        <div className={`floppyDisk-topper ${acColor} h-8 w-20 p-1 flex justify-end px-1 rounded-b-sm`}>
+      <div ref={containerRef} className={` flex ${bgColor} flex-col w-26 h-29 p-2 rounded-tr-xl rounded-sm gap-3 items-center pt-0 pb-0 box-border`}>
+        <div className={` ${acColor} h-8 w-20 p-1 flex justify-end px-1 rounded-b-sm`}>
           <div className="bg-[#C0C0C0] w-12 h-6 flex justify-end px-2 p-1">
             <div className={`${acColor} h-4 w-2`}></div>
           </div>
         </div>
-        <div className="floppyDisk-namePlace bg-yellow-50 h-17 w-20  pb-0 flex flex-col  justify-end items-center ">
+        <div className="bg-yellow-50 h-17 w-20  pb-0 flex flex-col  justify-end items-center ">
           <h3 className="floppyDisk-title">{title}</h3>
           
         </div>
